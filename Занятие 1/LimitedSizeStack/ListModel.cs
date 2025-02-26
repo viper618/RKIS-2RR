c196e0fd6dcb093f7bc355039e05957d572c78bf
GOL <164672521+accum104@users.noreply.github.com>
2025-02-26 22:46:28 +0300

    Shscherbinin. lesson 1: ListModel

--- Занятие 1/LimitedSizeStack/ListModel.cs
+++ Занятие 1/LimitedSizeStack/ListModel.cs
@@ -1,40 +1,78 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
-namespace LimitedSizeStack;
-
-public class ListModel<TItem>
+namespace LimitedSizeStack
 {
-	public List<TItem> Items { get; }
-	public int UndoLimit;
-        
-	public ListModel(int undoLimit) : this(new List<TItem>(), undoLimit)
-	{
-	}
+    public class ListModel<TItem>
+    {
+        public enum TypeAction
+        {
+            AddItem,
+            RemoveItem
+        }
 
-	public ListModel(List<TItem> items, int undoLimit)
-	{
-		Items = items;
-		UndoLimit = undoLimit;
-	}
+        public List<TItem> Items { get; private set; }
+        public int UndoLimit { get; private set; }
+        private LimitedSizeStack<Tuple<TypeAction, TItem, int>> StoryAction { get; set; }
 
-	public void AddItem(TItem item)
-	{
-		Items.Add(item);
-	}
+        public ListModel(int undoLimit)
+        {
+            Items = new List<TItem>();
+            UndoLimit = undoLimit;
+            StoryAction = new LimitedSizeStack<Tuple<TypeAction, TItem, int>>(undoLimit);
+        }
 
-	public void RemoveItem(int index)
-	{
-		Items.RemoveAt(index);
-	}
+        public ListModel(List<TItem> items, int undoLimit)
+        {
+            Items = items ?? throw new ArgumentNullException(nameof(items));
+            UndoLimit = undoLimit;
+            StoryAction = new LimitedSizeStack<Tuple<TypeAction, TItem, int>>(undoLimit);
+        }
 
-	public bool CanUndo()
-	{
-		return false;
-	}
+        public void AddItem(TItem item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
 
-	public void Undo()
-	{
-		throw new NotImplementedException();
-	}
+            StoryAction.Push(Tuple.Create(TypeAction.AddItem, item, Items.Count));
+            Items.Add(item);
+        }
+
+        public void RemoveItem(int index)
+        {
+            if (index < 0 || index >= Items.Count)
+            {
+                throw new IndexOutOfRangeException("Index is out of range.");
+            }
+
+            StoryAction.Push(Tuple.Create(TypeAction.RemoveItem, Items[index], index));
+            Items.RemoveAt(index);
+        }
+
+        public bool CanUndo()
+        {
+            return StoryAction.Count > 0;
+        }
+
+        public void Undo()
+        {
+            if (!CanUndo())
+            {
+                throw new InvalidOperationException("No actions to undo.");
+            }
+
+            var lastAction = StoryAction.Pop();
+            switch (lastAction.Item1)
+            {
+                case TypeAction.AddItem:
+                    Items.RemoveAt(lastAction.Item3);
+                    break;
+                case TypeAction.RemoveItem:
+                    Items.Insert(lastAction.Item3, lastAction.Item2);
+                    break;
+            }
+        }
+    }
 }
