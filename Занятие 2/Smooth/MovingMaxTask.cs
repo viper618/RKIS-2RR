4bd6deecd36af59086f7677011831c5d058a56b5
GOL <164672521+accum104@users.noreply.github.com>
2025-03-06 23:52:40 +0300

    Shcherbinin lesson 2 MovingMaxTask.cs

--- Занятие 2/Smooth/MovingMaxTask.cs
+++ Занятие 2/Smooth/MovingMaxTask.cs
@@ -1,14 +1,40 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-
 namespace yield;
-
 public static class MovingMaxTask
 {
 	public static IEnumerable<DataPoint> MovingMax(this IEnumerable<DataPoint> data, int windowWidth)
 	{
-		//Fix me!
-		return data;
+		if (windowWidth <= 0){
+            throw new ArgumentOutOfRangeException(nameof(windowWidth));
+		}
+
+        int Index = 0;
+
+        LinkedList<double> potentialMaxValues = new LinkedList<double>();
+        Queue<double> slidingWindow = new Queue<double>();
+
+        foreach (var point in data){
+            if (Index < windowWidth){
+                slidingWindow.Enqueue(point.OriginalY);
+                Index++;
+            }else{
+                if (potentialMaxValues.Count > 0 && potentialMaxValues.First.Value == slidingWindow.Dequeue()){
+                    potentialMaxValues.RemoveFirst();
+				}
+            }
+
+            slidingWindow.Enqueue(point.OriginalY);
+
+            while (potentialMaxValues.Count > 0 && potentialMaxValues.Last.Value <= point.OriginalY){
+                potentialMaxValues.RemoveLast();
+			}
+
+            potentialMaxValues.AddLast(point.OriginalY);
+
+            var smoothedPoint = point.WithMaxY(potentialMaxValues.First.Value);
+            yield return smoothedPoint;
+		}
 	}
 }
