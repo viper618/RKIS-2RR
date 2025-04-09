2750e7f8bde9208e1ceae3ef43c3f057f4a19157
GOL <164672521+accum104@users.noreply.github.com>
2025-04-09 22:21:16 +0300

    Shcherbinin. lesson 5 StatisticsTask.cs

--- Занятие 5/linq-slideviews/StatisticsTask.cs
+++ Занятие 5/linq-slideviews/StatisticsTask.cs
@@ -2,12 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace linq_slideviews;
-
-public class StatisticsTask
+namespace linq_slideviews
 {
-	public static double GetMedianTimePerSlide(List<VisitRecord> visits, SlideType slideType)
-	{
-		throw new NotImplementedException();
-	}
+    public class StatisticsTask
+    {
+        public static double GetMedianTimePerSlide(List<VisitRecord> visits, SlideType slideType)
+        {
+            return visits
+                .GroupBy(visit => visit.UserId)
+                .SelectMany(group => group
+                    .OrderBy(x => x.DateTime)
+                    .Select((visit, index) => new { visit, index }) // Добавляем индекс для получения пар
+                    .Where(x => x.index < group.Count() - 1) // Убедимся, что индекс не выходит за границы
+                    .Select(x => (x.visit, group.ElementAt(x.index + 1))) // Создаем кортеж
+                )
+                .Select(bigram => GetTime(bigram, slideType))
+                .Where(time => time >= 1 && time <= 120)
+                .DefaultIfEmpty(0)
+                .GetMedian(); // Используем метод GetMedian для получения медианы
+        }
+
+        private static double GetTime((VisitRecord First, VisitRecord Second) visits, SlideType slideType)
+        {
+            // Проверяем, что оба визита принадлежат одному пользователю и соответствуют типу слайда
+            if (visits.First.UserId.Equals(visits.Second.UserId) && visits.First.SlideType == slideType)
+            {
+                return visits.Second.DateTime.Subtract(visits.First.DateTime).TotalMinutes;
+            }
+            return 0; // Если условия не выполнены, возвращаем 0
+        }
+    }
 }
