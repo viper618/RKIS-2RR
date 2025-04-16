3170c90dfeadee021df34cbc6eca4d97625f9284
Bogdan Metelkin 3831 <simpl.top48786@gmail.com>
2025-04-16 23:30:50 +0300

    Lesson 6: BankServer

--- Занятие 6/Bank/BankServer.cs
+++ Занятие 6/Bank/BankServer.cs
@@ -6,22 +6,49 @@ using System.Threading.Tasks;
 namespace Bank
 {
 	public class BankServer
-	{
-		private readonly ConcurrentDictionary<Guid, BankAccount> _accounts = new();
+    {
+        private readonly ConcurrentDictionary<Guid, BankAccount> _accounts = new();
 
-		public async Task<Guid> CreateAccount(decimal initialBalance = 0)
-		{
-			throw new NotImplementedException();
-		}
+        public async Task<Guid> CreateAccount(decimal initialBalance = 0)
+        {
+            if (initialBalance < 0)
+            {
+                throw new ArgumentException("Initial balance cannot be negative", nameof(initialBalance));
+            }
 
-		public async Task PerformTransactionAsync(Guid fromAccountId, Guid toAccountId, decimal amount)
-		{
-			throw new NotImplementedException();
-		}
+            var account = new BankAccount();
+            await account.DepositAsync(initialBalance); // Начальный баланс
+            _accounts.TryAdd(account.Id, account);
+            return account.Id;
+        }
 
-		public async Task<decimal> GetAccountBalanceAsync(Guid accountId)
-		{
-			throw new NotImplementedException();
-		}
-	}
+        public async Task PerformTransactionAsync(Guid fromAccountId, Guid toAccountId, decimal amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentException("Transaction amount cannot be negative", nameof(amount));
+            }
+
+            if (!_accounts.ContainsKey(fromAccountId) || !_accounts.ContainsKey(toAccountId))
+            {
+                throw new KeyNotFoundException("One or both accounts do not exist.");
+            }
+
+            var fromAccount = _accounts[fromAccountId];
+            var toAccount = _accounts[toAccountId];
+
+            await fromAccount.WithdrawAsync(amount);
+            await toAccount.DepositAsync(amount);
+        }
+
+        public async Task<decimal> GetAccountBalanceAsync(Guid accountId)
+        {
+            if (!_accounts.ContainsKey(accountId))
+            {
+                throw new KeyNotFoundException("Account does not exist.");
+            }
+
+            return await Task.FromResult(_accounts[accountId].GetBalance());
+        }
+    }
 }
